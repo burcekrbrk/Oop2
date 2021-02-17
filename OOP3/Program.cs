56c94ff6c1a47437bc92763d419e7159dd9587f1
burçe <burcekarabork@gmail.com>
2021-02-17 03:50:22 +0300

    Add project files.

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();   //İhtiyacKrediManager yerine IkrediManager de dense aynı sonuç olur.
+            IKrediManager tasitKrediManager = new TasitKrediManager();          //TasitKrediManager yerine IkredManager denebilir.
+            IKrediManager konutKrediManager = new KonutKrediManager();         //KonutKrediManager yerine IkrediManager denebilir.
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new SmsLoggerService());
+
+            System.Collections.Generic.List<IKrediManager> krediler = new System.Collections.Generic.List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+        }
+    }
+}
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)        burada bu tarz kodlar yazılır, müşteri boşsa gibi bir kod yazılıcaktı ama musteri
+            //sınıfı boş olduğu için yazmayablrz
+            //{
+            //}
+        }
+    }
+}
